405f306c52855eeee724f0df42a2ef4e372da14d
Harsh <harsh.pradhan@ext.zestmoney.in>
2022-07-06 12:25:50 +0530

    Resolved 'Can't resolve ./App error'

--- src/components/DecrementButton.tsx
+++ src/components/DecrementButton.tsx
@@ -1,21 +1,22 @@
 interface Props {
-  decrementCount: Function;
-}
-
-const DecrementButton: React.FC<Props> = ({ decrementCount }) => {
-  return (
-    <button
-      style={{
-        height: "100px",
-        width: "70px",
-        fontSize: "60px",
-        marginRight: "10px",
-      }}
-      onClick={() => decrementCount((prevCount: number) => prevCount - 1)}
-    >
-      -
-    </button>
-  );
-};
-
-export default DecrementButton;
+    decrementCount: Function;
+  }
+  
+  const DecrementButton: React.FC<Props> = ({ decrementCount }) => {
+    return (
+      <button
+        style={{
+          height: "100px",
+          width: "70px",
+          fontSize: "60px",
+          marginRight: "10px",
+        }}
+        onClick={() => decrementCount((prevCount: number) => prevCount - 1)}
+      >
+        -
+      </button>
+    );
+  };
+  
+  export default DecrementButton;
+  
--- src/components/IncrementButton.tsx
+++ src/components/IncrementButton.tsx
@@ -1,21 +1,22 @@
 interface Props {
-  incrementCount: Function;
-}
-
-const IncrementCounter: React.FC<Props> = ({ incrementCount }) => {
-  return (
-    <button
-      style={{
-        height: "100px",
-        width: "70px",
-        fontSize: "60px",
-        marginLeft: "10px",
-      }}
-      onClick={() => incrementCount((prevCount: number) => prevCount + 1)}
-    >
-      +
-    </button>
-  );
-};
-
-export default IncrementCounter;
+    incrementCount: Function;
+  }
+  
+  const IncrementCounter: React.FC<Props> = ({ incrementCount }) => {
+    return (
+      <button
+        style={{
+          height: "100px",
+          width: "70px",
+          fontSize: "60px",
+          marginLeft: "10px",
+        }}
+        onClick={() => incrementCount((prevCount: number) => prevCount + 1)}
+      >
+        +
+      </button>
+    );
+  };
+  
+  export default IncrementCounter;
+  
